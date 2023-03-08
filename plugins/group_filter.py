6dc46e8f8d53fd12e81c6b10e419a407f8b9db38
rvaspentertainment <112102231+rvaspentertainment@users.noreply.github.com>
2023-03-09 00:07:50 +0530

    Update group_filter.py

--- plugins/group_filter.py
+++ plugins/group_filter.py
@@ -181,7 +181,7 @@ async def auto_filter(client, msg, spoll=False):
             **locals()
         )
     else:
-        cap = f"Here is what i found for your query {search}"
+        cap = f"How to download👉👉👉 https://t.me/how_to_open_links1"
     if imdb and imdb.get('poster'):
         try:
             hehe = await message.reply_photo(photo=imdb.get('poster'), caption=cap, reply_markup=InlineKeyboardMarkup(btn))
